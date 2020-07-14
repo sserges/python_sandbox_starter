8e8f4b4684a4960800ca0b6819e4ed58c41534e9
sserges <agounkeserge@gmail.com>
2020-07-14 19:42:20 +0000

    Tuples & Sets

--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,11 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
 
+fruit_tuple = ('apple', 'orange', 'mango')
 
+print(fruit_tuple[1])
+
+# Can not change value
+fruit_tuple[1] = 'Grape'
+
+# Tuples with one value should have trailing comma
 # A Set is a collection which is unordered and unindexed. No duplicate members.
