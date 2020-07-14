6bf419c4ec9e95a42114822ce890fa1f805f778e
sserges <agounkeserge@gmail.com>
2020-07-14 17:07:44 +0000

    Lists

--- lists.py
+++ lists.py
@@ -1,1 +1,9 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create list
+# numbers = [1, 2, 3, 4, 5]
+
+# Using a constructor
+numbers = list((1, 2, 3, 4, 5))
+
+print(numbers)
