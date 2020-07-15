db737eeaa02128c67cb7d827f436b9fdf1e42a9d
sserges <agounkeserge@gmail.com>
2020-07-15 18:57:43 +0000

    Dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,13 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+
+# Simple dict
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30
+}
+
+# Using a constructor
+person = dict(first_name='John', last_name='Doe', age=30)
+
+print(person)
