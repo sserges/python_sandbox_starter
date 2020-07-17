19ed08f22a87281e490c4e3e39a6da7c244a3933
sserges <agounkeserge@gmail.com>
2020-07-17 11:00:14 +0000

    modules

--- modules.py
+++ modules.py
@@ -1,1 +1,18 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+
+# Core modules
+import datetime
+
+# Pip modules
+import camelcase
+
+# Custom modules
+import validator
+
+today = datetime.date.today()
+
+camel = camelcase.CamelCase()
+text = 'hello there world'
+
+print(camel.hump(text))
